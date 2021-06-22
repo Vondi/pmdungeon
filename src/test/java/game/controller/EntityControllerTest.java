48446be7d9c102ec61f0a33cd679c26647c870a1
Timon Buschendorf <timon.buschendorf@fh-bielefeld.de>
2021-06-22 12:08:40 +0200

    some changes in build.gradle for testing and added one dummy test

---
+++ src/test/java/game/controller/EntityControllerTest.java
@@ -0,0 +1,20 @@
+package game.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class EntityControllerTest {
+
+    @Test
+    void test1() {
+        assertEquals(1,1);
+    }
+
+    @Test
+    void test2() {
+        assertEquals(2,2);
+    }
+
+}
